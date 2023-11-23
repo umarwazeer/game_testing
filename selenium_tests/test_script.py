0f3a7bb4083301813674280082379541ade3c6b3
umarkhan80 <umarkhan580830@gmail.com>
2023-11-23 14:36:42 +0500

    Add Selenium script and GitHub Actions

--- selenium_tests/test_script.py
+++ selenium_tests/test_script.py
@@ -15,11 +15,35 @@ os.environ['PATH'] += ';' + chromedriver_path
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options, executable_path=chromedriver_path)
 
 # Open the game URL
 driver.get("https://word-corners.nodehill.se/")
 
+try:
+    # Example: Wait for document ready state
+    WebDriverWait(driver, 120).until(
+        lambda driver: driver.execute_script('return document.readyState == "complete"')
+    )
+
+    # Find the file input field using its name attribute
+    file_input = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'file'))
+    )
+
+    # Unhide the file input field using JavaScript
+    driver.execute_script("arguments[0].style.display = 'block';", file_input)
+
+    # Set the file path and send keys to the file input field
+    file_input.send_keys("path/to/your/file.txt")
+
+    # Continue with other actions based on your game testing progress
+
+finally:
+    # Add any cleanup steps or close the browser window after testing
+    driver.quit()
+
+    
 try:
     WebDriverWait(driver, 120).until(
     lambda driver: driver.execute_script('return document.readyState == "complete"')
